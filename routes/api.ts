16876d6bf3f92cdc93d1a3b860749542c4386294
Antonious-Stewart <antonious.stewart@gmail.com>
2023-02-12 15:12:20 -0500

    remove rest endpoints

--- routes/api.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import customerRouter from '../customers/api/customer-route';
-
-const router = Router();
-
-router.use('/api/customers', customerRouter);
-
-export default router;
